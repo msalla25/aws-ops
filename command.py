bc190f4272d2ee772d92ffaf774ef1557aff9d18
msalla25 <sallamahidhar26@gmail.com>
2026-05-31 14:32:35 -0400

    Update command.py for survey migration options

--- command.py
+++ command.py
@@ -1,7 +1,7 @@
 python3 aap_migrate_surveys_schedules.py \
     --src-host https://aap24.yourco.com \
     --dst-host https://aap25.yourco.com \
-    --src-org "Old Org Name" --dst-org "New Org Name" \
-    --insecure              # dry run
-
-# then add --commit
+    --src-org "Old Org Name" \
+    --dst-org "New Org Name" \
+    --only surveys \
+    --insecure
