055679f7a9b30bc797f4124ee8bf5cc18370f1ca
msalla25 <sallamahidhar26@gmail.com>
2026-05-31 14:30:40 -0400

    Modify organization names and improve comments
    Updated organization names for migration script and added comments for clarity.

--- command.py
+++ command.py
@@ -1,12 +1,7 @@
-# Keeps tokens out of shell history
-export AAP_SRC_TOKEN='source-pat'
-export AAP_DST_TOKEN='dest-pat'
-
-# Dry run
 python3 aap_migrate_surveys_schedules.py \
     --src-host https://aap24.yourco.com \
     --dst-host https://aap25.yourco.com \
-    --org "My Organization" --insecure
+    --src-org "Old Org Name" --dst-org "New Org Name" \
+    --insecure              # dry run
 
-# Apply
-python3 aap_migrate_surveys_schedules.py ... --org "My Organization" --commit --insecure
+# then add --commit
